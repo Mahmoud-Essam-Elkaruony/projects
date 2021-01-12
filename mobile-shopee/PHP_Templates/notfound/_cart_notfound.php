b0a6132e32931a6af540157c1e37dcbece7bc9d1
Mahmoud_Essam <65455064+Mahmoud-Essam-Elkaruony@users.noreply.github.com>
2021-01-13 01:15:02 +0200

    Add files via upload

---
+++ mobile-shopee/PHP_Templates/notfound/_cart_notfound.php
@@ -0,0 +1,36 @@
+<!--shopping cart section -->
+<section id="cart-not-found" class="py-3 mb-5">
+
+    <div class="container-fluid">
+        <h5 class="font-baloo font-size-20 text-center">Empty Cart</h5>
+        <!--shopping cart item-->
+        <div class="row">
+
+            <div class="col-sm-9">
+                <!--Empty Cart-->
+                <div class="row py-3 mt-3">
+                    <div class="col-sm-12 text-center py-2">
+                        <img src="./assets/blog/empty_cart.png" alt="Empty Cart" class="img-fluid" style="height: 200px">
+                        <p class="font-baloo font-size-16 text-black-50">Empty Cart</p>
+                    </div>
+                </div>
+                <!--#Empty Cart-->
+            </div>
+
+            <!--Subtotal sectaion-->
+            <div class="col-sm-3 d-flex h-50 px-3">
+                <div class="sub-total border text-center mt-2">
+                    <h6 class="font-size-12 font-raleway text-success py-3"><i class="fas fa-check"></i>Your order is eligible for FREE Delivery</h6>
+                    <div class="border-top py-4">
+                        <h5 class="font-baloo font-size-20">Subtotal(item-1)</h5>
+                        <button type="submit" class="btn btn-warning mt-3">Proceed to Buy</button>
+                    </div>
+                </div>
+            </div>
+            <!--#Subtotal sectaion-->
+        </div>
+        <!--end shopping cart item-->
+    </div>
+
+</section>
+<!--end shopping cart section -->
